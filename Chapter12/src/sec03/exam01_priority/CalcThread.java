2698d5161f94b5189beb49503d1be922f9293b48
kiyoung85 <kiyoung85@gmail.com>
2020-08-06 01:04:42 +0900

    Thread 우선 순위

---
+++ Chapter12/src/sec03/exam01_priority/CalcThread.java
@@ -0,0 +1,16 @@
+package sec03.exam01_priority;
+
+public class CalcThread extends Thread {
+
+    public CalcThread(String name) {
+        setName(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 2000000000; i++) {
+
+        }
+        System.out.println(getName());
+    }
+}
