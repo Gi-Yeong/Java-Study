9b359dab357086c4fd6cee96c4c52f5af931cb23
kiyoung85 <kiyoung85@gmail.com>
2020-08-19 22:04:23 +0900

    표준 API 의 함수적 인터페이스 #3

---
+++ Chapter14/src/sec05/exam07_andthen_compose/PredicateIsEqualExample.java
@@ -0,0 +1,21 @@
+package sec05.exam07_andthen_compose;
+
+import java.util.function.Predicate;
+
+public class PredicateIsEqualExample {
+
+    public static void main(String[] args) {
+        Predicate<String> predicate;
+        predicate = Predicate.isEqual(null);
+        System.out.println("null, null: " + predicate.test(null));
+
+        predicate = Predicate.isEqual("Java8");
+        System.out.println("null, Java8: " + predicate.test(null));
+
+        predicate = Predicate.isEqual("null");
+        System.out.println("null, Java8: " + predicate.test("Java8"));
+
+        predicate = Predicate.isEqual("Java8");
+        System.out.println("Java8, Java8: " + predicate.test("Java8"));
+    }
+}
---
+++ Chapter14/src/sec05/exam08_and_or_negate_isequal/PredicateAndOrNegateExample.java
@@ -0,0 +1,32 @@
+package sec05.exam08_and_or_negate_isequal;
+
+import java.util.function.IntPredicate;
+
+public class PredicateAndOrNegateExample {
+
+    public static void main(String[] args) {
+        // 2의 배수를 검사
+        IntPredicate predicateA = a -> a % 2 == 0;
+
+        // 3의 배수를 검사
+        IntPredicate predicateB = a -> a % 3 == 0;
+
+        IntPredicate predicateAB;
+        boolean result;
+
+        //and()
+        predicateAB = predicateA.and(predicateB);
+        result = predicateAB.test(9);
+        System.out.println("9는 2와 3의 배수 입니까? " + result);
+
+        //or()
+        predicateAB = predicateA.or(predicateB);
+        result = predicateAB.test(9);
+        System.out.println("9는 2와 3의 배수 입니까? " + result);
+
+        //negate()
+        predicateAB = predicateA.negate();
+        result = predicateAB.test(9);
+        System.out.println("9는 홀수입니까? " + result);
+    }
+}
