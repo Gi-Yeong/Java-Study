abe5829ed1ffd6b03ef7bcf3ffe25d80a7b33199
kiyoung85 <kiyoung85@gmail.com>
2020-09-10 14:51:29 +0900

    Java.time 패키지#5

---
+++ Chapter11/src/sec16/exam01_java_time/DateTimeFormatExample.java
@@ -0,0 +1,16 @@
+package sec16.exam01_java_time;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatExample {
+
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 a h시 m분 s초");
+
+        String nowString = now.format(dateTimeFormatter);
+        System.out.println(nowString);
+    }
+}
